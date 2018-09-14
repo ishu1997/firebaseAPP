1d1b8d8c57fed09620c3d1fe7a10efbb5fd42640
niyoktaHR <nyoktahr@gmail.com>
2018-09-14 11:56:51 +0530

    calendar fault

--- app/src/main/java/com/example/shubham/firebaseapp/Form_fragments/fragment_form_prefrences.java
+++ app/src/main/java/com/example/shubham/firebaseapp/Form_fragments/fragment_form_prefrences.java
@@ -21,6 +21,11 @@ public class fragment_form_prefrences extends Fragment {
     View view;
     ViewPager viewPager;
     Button done_btn;
+    Spinner spin_Location ;
+    Spinner spin_job_1 ;
+    Spinner spin_job_2 ;
+    Spinner spin_job_3 ;
+
 
     public fragment_form_prefrences() {
 
@@ -36,21 +41,21 @@ public class fragment_form_prefrences extends Fragment {
 
         done_btn = view.findViewById(R.id.form_preferences_doneBTN);
 
-        Spinner spin_category = view.findViewById(R.id.job_caetgories);
-        Spinner spin_job_1 = view.findViewById(R.id.job_prefer_1);
-        Spinner spin_job_2 = view.findViewById(R.id.job_prefer_2);
-        Spinner spin_job_3 = view.findViewById(R.id.job_prefer_3);
+         spin_Location = view.findViewById(R.id.job_caetgories);
+         spin_job_1 = view.findViewById(R.id.job_prefer_1);
+         spin_job_2 = view.findViewById(R.id.job_prefer_2);
+         spin_job_3 = view.findViewById(R.id.job_prefer_3);
 
 
         String[] categories = {"chooose here","Accounting Jobs" ,"Interior Design Jobs" ,"Bank Jobs" ,"Content Writing Jobs" ,"Consultant Jobs" ,"Engineering Jobs" ,"Export Import Jobs" ,"Merchandiser Jobs" ,"Security Jobs" ,"HR Jobs" ,"Hotel Jobs" ,"Application Programming Jobs" ,"Client Server Jobs", "DBA Jobs" ,"Ecommerce Jobs", "ERP Jobs", "VLSI Jobs" ,"Mainframe Jobs", "Middleware Jobs" ,"Mobile Jobs", "Network administrator Jobs" ,"IT Jobs" ,"Testing Jobs System", "Programming Jobs" ,"EDP Jobs Telecom" ,"Software Jobs", "Telecom Jobs", "BPO Jobs" ,"Legal Jobs" ,"Marketing Jobs" ,"Packaging Jobs" ,"Pharma Jobs" ,"Maintenance Jobs", "Logistics Jobs" ,"Sales Jobs" ,"Secretary Jobs"};
         ArrayAdapter<String> adapter_categories = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item,categories);
         adapter_categories.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
-        spin_category.setAdapter(adapter_categories);
+        spin_Location.setAdapter(adapter_categories);
 
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.Jobs_preferences_array));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item,categories);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
         spin_job_1.setAdapter(adapter);
@@ -64,11 +69,51 @@ public class fragment_form_prefrences extends Fragment {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getActivity(),"success" , Toast.LENGTH_SHORT).show();
+                if(is_filled()){
+
+                    Toast.makeText(getActivity(),"success" , Toast.LENGTH_SHORT).show();
+
+                    // add frirebase code here to move data to server
+
+                    // and also to start next acitvity
+
+
+                }
+
+                else{
+
+                    Toast.makeText(getActivity(),"fill all fields" , Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
 
 
         return view;
+    }
+
+
+    private boolean is_filled(){
+
+        if(spin_Location.getSelectedItem().toString()=="chooose here"){
+            return false;
+        }
+
+        if(spin_job_1.getSelectedItem().toString()=="chooose here"){
+            return false;
+        }
+
+        if(spin_job_2.getSelectedItem().toString()=="chooose here"){
+            return false;
+        }
+
+        if(spin_job_3.getSelectedItem().toString()=="chooose here"){
+            return false;
+        }
+
+
+
+        return true;
     }
 }
